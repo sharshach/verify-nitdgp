9434bb8c1e91430951353b0807e416ac64137199
Sri Harsha Chilukuri <sharshach@gmail.com>
2021-11-12 22:58:41 +0530

    scanner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import Report from './Report';
+import Report from './components/Report';
 import data from './text.json';
 import Codes from './components/Codes';
-
+import Scan from './components/Scan';
+ 
 function App() {
     var roll=new URLSearchParams(window.location.search).get("roll");
     console.log(roll);
+    if(window.location.pathname==="/scan"){
+        return (
+            <div className="App">    
+                <Scan/>
+            </div>
+        )
+    }
     if(roll==null){
         return (
             <div className="App">    
